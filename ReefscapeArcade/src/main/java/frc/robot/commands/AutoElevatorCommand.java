955b5da48e5fd1b05acf77549852e5a32434512c
Mira Xu <108564448+kiamimirax@users.noreply.github.com>
2025-01-31 19:34:07 -0500

    added elevator codes

---
+++ ReefscapeArcade/src/main/java/frc/robot/commands/AutoElevatorCommand.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class AutoElevatorCommand extends Command {
+  /** Creates a new ElevatorPositionCommand. */
+  ElevatorSubsystem m_elevatorSubsystem;
+  double m_dist;
+  String m_direction;
+
+  public AutoElevatorCommand(ElevatorSubsystem elevatorSubsystem, double dist, String direction) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_elevatorSubsystem = elevatorSubsystem;
+    m_dist = dist;
+    m_direction = direction;
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_elevatorSubsystem.resetEncoder();
+  //reset encoders
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_elevatorSubsystem.setSpeed(0.5, m_direction);
+    //run motor in specified direction
+  
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_elevatorSubsystem.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+
+    //check if encoder value is greater than or equal to distance inputted
+    
+    //without tolerance
+    if(m_elevatorSubsystem.getEncoderValue()>=m_dist)return true;
+    else return false;
+
+    // 2 is the tolerance
+   // if(Math.abs(m_elevatorSubsystem.getEncoderValue() - m_dist) < 0.5) return true;
+   // else return false;
+ 
+  }
+}
---
+++ ReefscapeArcade/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,77 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+//gracescommet
+
+// Natalie's test comment
+package frc.robot.subsystems;
+
+import frc.robot.MechanismConstants;
+import frc.robot.MechanismConstants.ElevatorConstants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import java.lang.reflect.Type;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig;
+
+public class ElevatorSubsystem extends SubsystemBase {
+  SparkMax elevatorMotor = new SparkMax(ElevatorConstants.kElevatorPort, MotorType.kBrushless);
+  SparkMaxConfig config = new SparkMaxConfig();
+  RelativeEncoder elevatorEncoder = elevatorMotor.getEncoder(); //42
+  
+
+
+  /** Creates a new ElevatorSubsystem. */
+  public ElevatorSubsystem() {
+
+    config
+    //.inverted(true)
+    .idleMode(IdleMode.kBrake);
+    config.encoder
+    .positionConversionFactor(ElevatorConstants.kElevatorEncoderConvFact);
+    // config.closedLoop
+    // .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+    // .pid(1.0, 0.0, 0.0);
+
+  }
+
+
+  public void resetEncoder(){
+    elevatorMotor.getEncoder().setPosition(0);
+  }
+
+  public double getEncoderValue(){
+    return elevatorMotor.getEncoder().getPosition();
+  }
+
+  public void stop(){
+    elevatorMotor.stopMotor();
+  }
+
+  public void setSpeed(double speed, String direction) {
+
+    if (direction.equals("up")) {
+      elevatorMotor.set(speed);
+    } 
+    else if (direction.equals("down")) {
+    elevatorMotor.set(speed*-1);
+    }
+  }
+
+
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
